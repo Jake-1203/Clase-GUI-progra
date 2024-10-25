fefc5ef022535d455eea675fef965a3ccfb7f528
Jake Kisler Mc Farlane Ellis <c24682@ecci.ucr.ac.cr>
2024-10-25 17:52:10 -0600

    Ventana Principal Punto 5

--- VentanaPrincipal.java
+++ VentanaPrincipal.java
@@ -1,6 +1,9 @@
 
+import java.io.FileWriter;
+
 import javax.swing.*;
 public class VentanaPrincipal extends JFrame {
+
     public VentanaPrincipal(){
         setTitle("Formulario de Usuario");
         setSize(900, 850);
@@ -19,9 +22,50 @@ public class VentanaPrincipal extends JFrame {
         menuBar.add(menuArchivo);
         setJMenuBar(menuBar);
 
+        salirItem.addActionListener(e -> System.exit(0));
+        
+        JPanel panelFormulario = new JPanel();
+        panelFormulario.setLayout(new BoxLayout(panelFormulario, BoxLayout.Y_AXIS));
+
+        JLabel etiquetaNombre = new JLabel("Nombre:");
+        JTextField campoNombre =  new JTextField(20);
+
+        JLabel etiquetaEmail = new JLabel("Email:");
+        JTextField campoEmail = new JTextField(20);
+
+        JLabel etiquetaTelefono = new JLabel("Telefono:");
+        JTextField campoTelefono = new JTextField(20);
+
+        panelFormulario.add(etiquetaNombre);
+        panelFormulario.add(campoNombre);
+        panelFormulario.add(etiquetaEmail);
+        panelFormulario.add(campoEmail);
+        panelFormulario.add(etiquetaTelefono);
+        panelFormulario.add(campoTelefono);
+        add(panelFormulario);
+
+        /*JButton botonGuardar = new JButton("Guardar");
+
+        botonGuardar.addActionListener(e ->{
+
+            String nombre = campoNombre.getText();
+            String email = campoEmail.getText();
+            String telefono = campoTelefono.getText();
+
+            try {
+                FileWriter = new FileWriter("datos_usuario",true);
+                writer.write("Nombre: " + nombre + ", Email:" + email + ", Telefono:" + telefono);
+                writer.close();
+                JOptionPane.showMessageDialog(null, "Datos guardados exitosamente.");
+            } catch (IOException ex) {
+                JOptionPane.showMessageDialog(null, "Error al guardar los datos.");
+                
+            }
+
+        });
         
 
-    }
+    }*/
 
     public static void main(String[] args) {
         VentanaPrincipal ventana = new VentanaPrincipal();
